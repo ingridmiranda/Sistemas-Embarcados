965f1e3195358034d7664582ec6264ee644fcb72
ingridmiranda <ingridmsousa@hotmail.com>
2017-08-18 12:04:26 -0300

    Add files via upload

---
+++ Respostas das aulas/aula_03/ola_usuario_1.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main (){
+   char nome_usuario[10];
+   printf("Digite seu nome: ");
+   scanf("%s", nome_usuario);
+   printf("Olá, %s \n", nome_usuario);
+   return 0;
+}
+
