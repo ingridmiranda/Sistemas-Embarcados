129b5633f7ffd26c6905037be84c91a706352559
Ingrid Miranda <ingridmsousa@hotmail.com>
2017-11-23 01:44:00 -0200

    PC4 atualizado

---
+++ Pontos de Controle/PC4/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PDF_NAME="RelatorioPC4JoaoIngrid.pdf"
+
+pdflatex relatorio.tex
+bibtex relatorio.aux
+pdflatex relatorio.tex
+pdflatex relatorio.tex
+
+rm -f *~ *.dvi *.ps *.backup *.aux *.log
+rm -f *.lof *.lot *.bbl *.blg *.brf *.toc *.idx
+
+mv relatorio.pdf $PDF_NAME
